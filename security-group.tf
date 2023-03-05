dd886a2067c9afa50b055a7f3b8455584d2ddf8d
AbdelrhmanAmen <abdelrhman_20180321@fci.helwan.edu.eg>
2023-03-05 23:02:35 +0200

    error modification

--- security-group.tf
+++ security-group.tf
@@ -5,7 +5,7 @@ data "aws_vpc" "vpc" {
 resource "aws_security_group" "allow_http_https" {
     name        = "allow http and https"
     description = "Allow port 80 for http and port 443 for https"
-    vpc_id      = data.aws_vpc.vpc
+    vpc_id      = data.aws_vpc.vpc.id
 
     ingress {
     description      = "allow HTTP inbound"
--- web-instance.tf
+++ web-instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "application" {
-    ami           = var.ami
-    instance_type = var.instance_type
+    ami           = var.image
+    instance_type = var.type
     vpc_security_group_ids = [aws_security_group.allow_http_https.id]
     subnet_id              = var.subnet_id
     tags = {
